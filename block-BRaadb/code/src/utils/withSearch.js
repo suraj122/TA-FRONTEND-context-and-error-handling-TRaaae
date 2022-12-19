3826d445535f8fb9d46a2f5203cfd59717359755
suraj_122 <122suraj0@gmail.com>
2022-12-19 17:17:52 +0530

    implemented search using HOC

---
+++ block-BRaadb/code/src/utils/withSearch.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function withSearch(Comp) {
+  return class extends React.Component {
+    state = {
+      searchTerm: "",
+    };
+    handleChange = ({ target: { value } }) => {
+      this.setState({ searchTerm: value });
+    };
+    render() {
+      return (
+        <Comp search={this.state.searchTerm} handleChange={this.handleChange} />
+      );
+    }
+  };
+}
+
+export default withSearch;
